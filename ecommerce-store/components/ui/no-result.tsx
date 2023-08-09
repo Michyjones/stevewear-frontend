7b7c0e65327d6db6ee8e3fc9b1f4967c3e6be83b
Michyjones <mykembuguah11@gmail.com>
2023-08-10 01:47:56 +0300

    Added a page to display when there are no results

---
+++ ecommerce-store/components/ui/no-result.tsx
@@ -0,0 +1,8 @@
+const NoResults = () => {
+    return ( <div className="flex items-center justify-center h-full w-full text-neutral-400">
+        No results found.
+
+    </div> );
+}
+ 
+export default NoResults;
