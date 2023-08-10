3ea55c5fa7dc0ca2101f6454317e1b43f758a102
Michyjones <mykembuguah11@gmail.com>
2023-08-10 17:37:29 +0300

    Added the routes to fetch single and all products

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -4,13 +4,18 @@ import { Product } from "@/types";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
   data: Product;
 }
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  }
   return (
-    <div onClick={() => {}} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
     <div className="aspect-square rounded-xl bg-gray-100 relative">
       <Image 
         src={data.images?.[0]?.url} 
